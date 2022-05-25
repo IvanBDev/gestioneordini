5ddc0191a287839e11b1ae0a65c2b9bf2d6f6519
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-25 09:10:12 +0200

    implemetato aggiorna

--- src/main/java/it/prova/gestioneordini/service/OrdineServiceImpl.java
+++ src/main/java/it/prova/gestioneordini/service/OrdineServiceImpl.java
@@ -68,7 +68,27 @@ public class OrdineServiceImpl implements OrdineService{
 	@Override
 	public void aggiorna(Ordine ordineInput) throws Exception {
 		// TODO Auto-generated method stub
-		
+		// questo è come una connection
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			// questo è come il MyConnection.getConnection()
+			entityManager.getTransaction().begin();
+
+			// uso l'injection per il dao
+			ordineDAO.setEntityManager(entityManager);
+
+			// eseguo quello che realmente devo fare
+			ordineDAO.update(ordineInput);
+
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
