e077dfff9779ba1c28c6b46a98a22262bcfcaa4d
VaXinNavi <ivan_bendotti@yahoo.com>
2022-05-25 10:23:17 +0200

    implemetato setCategoriaDAO + listAll

--- src/main/java/it/prova/gestioneordini/service/CategoriaServiceImpl.java
+++ src/main/java/it/prova/gestioneordini/service/CategoriaServiceImpl.java
@@ -2,23 +2,42 @@ package it.prova.gestioneordini.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import it.prova.gestioneordini.dao.EntityManagerUtil;
 import it.prova.gestioneordini.dao.categoria.CategoriaDAO;
 import it.prova.gestioneordini.model.Articolo;
 import it.prova.gestioneordini.model.Categoria;
 import it.prova.gestioneordini.model.Ordine;
 
-public class CategoriaServiceImpl implements CategoriaService{
+public class CategoriaServiceImpl implements CategoriaService {
 
+	private CategoriaDAO categoriaDAO;
+	
 	@Override
-	public void setCategoriaDAO(CategoriaDAO categoriaDaoInstance) throws Exception {
+	public void setCategoriaDAO(CategoriaDAO categoriaDAO) throws Exception {
 		// TODO Auto-generated method stub
-		
+		this.categoriaDAO = categoriaDAO;
 	}
 
 	@Override
 	public List<Categoria> listAll() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		// questo è come una connection
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			// uso l'injection per il dao
+			categoriaDAO.setEntityManager(entityManager);
+
+			// eseguo quello che realmente devo fare
+			return categoriaDAO.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
 	}
 
 	@Override
@@ -30,25 +49,25 @@ public class CategoriaServiceImpl implements CategoriaService{
 	@Override
 	public void aggiorna(Categoria categoriaInstance) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void inserisciNuovo(Categoria categoriaInstance) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void rimuovi(Long idGenere) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void aggiungiCd(Categoria categoriaInstance, Articolo articoloInstance) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
